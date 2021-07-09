09ae5a0d1187293267cd829c5149a2a30c61b3d0
Jon Dalberg <jon@releasehub.com>
2021-07-09 11:49:08 -0500

    props

--- app/index.php
+++ app/index.php
@@ -1,3 +1,3 @@
 <?php 
-    echo '<img src="trinity.jpg" alt="Hello GEE CEE PEE!" style="width: 100%"/>'; 
+    echo '<html><head><meta property="og:title" content="Them Guys"><meta property="og:description" content="These are people who made the things you have. Enjoy!"><meta property="og:image" content="trinity.jpg"></head><body><img src="trinity.jpg" alt="Hello GEE CEE PEE!" style="width: 100%"/></body></html>'; 
 ?> 
