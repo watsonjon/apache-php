12c4b455333dff6a0368feaa9494a41411f1114c
Jon Dalberg <jon@releasehub.com>
2022-02-08 15:11:00 -0600

    add health page

---
+++ healthz.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Healthz</title>
+    </head>
+    <body>
+        This is a healthz page.
+    </body>
+</html>
+
